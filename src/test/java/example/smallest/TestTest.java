32a830a408854e86f4ec78a0b1357f25dc508b6e
Shreya N M <76606579+shreyasnm9@users.noreply.github.com>
2024-09-20 20:43:03 +0530

    code change

---
+++ src/test/java/example/smallest/TestTest.java
@@ -0,0 +1,13 @@
+package example.smallest;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestTest {
+
+    @Test
+    public void testInstanceCreation() {
+        
+    }
+}
